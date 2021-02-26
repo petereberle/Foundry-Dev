7248949d8d75c283c4a157b7530e7d935e9c7f12
Peter Eberle <46387202+petereberle@users.noreply.github.com>
2021-02-26 15:55:33 -0800

    exploded version

--- Menu_Overlay/scripts.js
+++ Menu_Overlay/scripts.js
@@ -3,13 +3,17 @@ $(document).ready(function (){
 
 	$("#hamburger").on("click", function(){
 
-		$('#side_menu_wrapper').animate({marginRight: '100vw'}, 500);
+		$('#side_menu_wrapper').css('display', 'block');
+
+		$("#hamburger").css('opacity', '0');
 
 		// $('header').css('borderBottom' , 'solid .012px white');
 
+		$('#menu_side').animate({opacity: '1'}, 700);
+
 		$('#side_menu_wrapper').addClass("active");
 
-		$('#content').css('filter', 'blur(5px)');
+		$('#content').css('filter', 'blur(5px) opacity(25%)');
 
 	});
 
@@ -17,7 +21,11 @@ $(document).ready(function (){
 
 	if ( $('#side_menu_wrapper').hasClass('active') ){
 
-		$('#side_menu_wrapper').animate({marginRight: '0'}, 500);
+		$('#menu_side').animate({opacity: '0'}, 700);
+
+		$("#hamburger").css('opacity', '1');
+
+		$('#side_menu_wrapper').css('display', 'none');
 
 		// $('header').css('borderBottom' , 'solid .012px #bebec1');
 
---
+++ On_Click_Explode/scripts.js
@@ -0,0 +1,75 @@
+$(document).ready(function (){
+
+	$("#hamburger").on("click", function(){
+
+		$('#side_menu_wrapper').animate({marginRight: '100vw'}, 500);
+
+		// $('header').css('borderBottom' , 'solid .012px white');
+
+		$('#side_menu_wrapper').addClass("active");
+
+		$('#content').css('filter', 'blur(5px)');
+
+	});
+
+	$("#xit").on("click", function(){
+
+	if ( $('#side_menu_wrapper').hasClass('active') ){
+
+		$('#side_menu_wrapper').animate({marginRight: '0'}, 500);
+
+		// $('header').css('borderBottom' , 'solid .012px #bebec1');
+
+		$('#content').css('filter', 'blur(0px)');
+
+	}
+
+	});
+
+
+
+// Open Project Details
+
+var winHeight = $(window).height() - 50;
+var winWidth = $(window).width() - 50;
+
+$('#content').on("click", function(){
+
+	$('#content').css('position', 'relative');
+	$('#content').animate({padding: '25px'});
+
+	$('#content').addClass('reduced');
+
+	$('header').css('position', 'relative');
+	$('#slide_wrapper').animate({width: '85%'});
+	$('#info_wrapper').animate({opacity: '1'});
+	$('#info_wrapper').css('display', 'flex');
+
+	$("#hamburger").css('filter', 'invert(1)');
+	$("#logo").css('filter', 'invert(1)');
+
+
+	//undo
+
+	if ( $('#content').hasClass('reduced') ) {
+
+	// $('#content').css('position', 'absolute');
+	// $('#content').animate({padding: '0'});
+
+	// $('#content').removeClass('reduced');
+
+	// $('header').css('position', 'absolute');
+	// $('#slide_wrapper').animate({width: '100%'});
+	// $('#info_wrapper').animate({opacity: '0'});
+	// $('#info_wrapper').css('display', 'none');
+
+	// $("#hamburger").css('filter', 'invert(0)');
+	// $("#logo").css('filter', 'invert(0)');
+
+	}
+
+});
+
+
+
+});
